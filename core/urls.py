19b102f4c0d45bea8cf15d7d7e2daf2081d38035
NewProject <uzbzarif@gmail.com>
2024-02-24 14:33:57 +0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -19,10 +19,6 @@ from api.views import add_product, get_all_product, get_product, delete_product,
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('add/', add_product),
-    path('get/', get_all_product),
-    path('get/<int:pk>', get_product),
-    path('del/<int:pk>', delete_product),
-    path('update/<int:pk>', update_product),
+
 ]
  
