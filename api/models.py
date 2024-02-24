50d6306379486ffdce1794b912017a6613cc42ef
NewProject <uzbzarif@gmail.com>
2024-02-24 14:32:52 +0500

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,26 +1,6 @@
 from django.db import models
 
 class SmartPhone(models.Model):
-    price = models.FloatField()
-    img_url = models.CharField(max_length=255)
-    color = models.CharField(max_length=30)
-    ram = models.IntegerField()
-    memory = models.IntegerField()
-    name = models.CharField(max_length=30)
-    model = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.name
-    
-    def to_dict(self):
-        returned = {
-            'id': self.id,
-            'price': self.price,
-            'img_url':self.img_url,
-            'color':self.color,
-            'ram':self.ram,
-            'memory':self.memory,
-            'name':self.name,
-            'model':self.model
-        }
-        return returned
+    """
+    Model
+    """
